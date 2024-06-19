1b7baf675a41a956202495c798f203b01ea91ae8
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-06-19 09:42:25 +0530

    User can submit only once

--- client/src/Components/CurrentQuestion.tsx
+++ client/src/Components/CurrentQuestion.tsx
@@ -6,6 +6,7 @@ type currentQuestionProps = {
 
 const CurrentQuestion = ({ question }: currentQuestionProps) => {
     const [selectedOption, setSelectedOption] = useState("")
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
     return (
         <div className="flex flex-col items-center w-full h-[100vh]">
             <h1 className="mt-[40px] text-[40px] font-bold">Quizzily</h1>
@@ -27,7 +28,7 @@ const CurrentQuestion = ({ question }: currentQuestionProps) => {
                     })}
                 </div>
             </div>
-            <button className="bg-[#4374a1] text-white text-[18px] hover:opacity-90 mt-6 px-4 py-2 rounded-xl font-medium">Submit</button>
+            <button disabled = {isSubmitted} className="bg-[#4374a1] text-white text-[18px] hover:opacity-90 mt-6 px-4 py-2 rounded-xl font-medium">Submit</button>
         </div>
     )
 }
